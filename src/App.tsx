767df21183dd111081b3cbd90fa141dc9de7589f
NastjaFedina <148310731+NastjaFedina@users.noreply.github.com>
2026-03-23 18:58:33 +0200

    Import CharactersPage and CharacterDetail components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import CharactersPage from './components/CharactersPage'
+import CharacterDetail from './components/CharacterDetail'
 
 function App() {
   return (
